36d138adc27c45472959a12ad8565aaf73e9cfd1
niiick1 <nicolas.cnunes@gmail.com>
2018-03-06 01:56:27 -0800

    Raise number of transactions per page to 10

--- app/config/middleware/pagination-handler.js
+++ app/config/middleware/pagination-handler.js
@@ -2,7 +2,7 @@ const paginate = require('express-paginate'),
   paginationLinksAppender = require('./pagination-links-appender')
 
 module.exports = getTotalRecordsService => (req, res, next) => {
-  paginate.middleware(1, 100)(req, res, error => {
+  paginate.middleware(10, 100)(req, res, error => {
     if (error) {
       next(error)
     }
